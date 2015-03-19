5f5caa9f8bb619ab933238526301dbd0a60e5917
Ali.Dbg <ali.debugger@gmail.com>
2015-03-19 10:09:43 +0330

    version 2.0
    update to version 2.0

--- readme.txt
+++ readme.txt
@@ -1,64 +1,73 @@
 ﻿=== WP-Farsi ===
 Contributors: Ali.Dbg
-Tags: wp, date,time,iran,jalali,shamsi,hijri,hejri,calendar,gregorian,conversion,khorshidi,solar,persian,parsi,farsi,RTL,fa_IR شمسی,خورشیدی,تاریخ,تقویم,هجری,جلالی,مبدل,پلاگین,افزونه,وردپرس,فارسی,پارسی,ایران
+Tags:wp,date,time,iran,jalali,shamsi,hijri,hejri,calendar,gregorian,language,translation,locale,mysql2date,date_i18n,conversion,khorshidi,solar,persian,parsi,farsi,RTL,i18n,fa_IR,fa-IR,شمسی,خورشیدی,تاریخ,تقویم,هجری,جلالی,مبدل,پلاگین,افزونه,وردپرس,فارسی,پارسی,ایران
 Requires at least: 3.0.1
 Tested up to: 4.2
 Stable tag: 4.0
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
-تاریخ‌ شمسی, Conversion Gregorian date to Solar, time jalali persian parsi farsi RTL calendar wp plugin iran shamsi khorshidi hejri unicode
+تبدیل تاریخ میلادی وردپرس به شمسی Gregorian date to Solar, time jalali persian parsi farsi wp iran shamsi khorshidi hejri
 
 == Description ==
 
-= مبدل تاریخ میلادی وردپرس به خورشیدی =
 
-
-> این پلاگین از دو ویجت تقویم+بایگانی و پیوندیکتا خورشیدی پشتیبانی نمی‌کند در صورت نیاز میتوانید از امکانات پلاگین‌های جلالی و پارسی‌دیت در کنار این پلاگین استفاده کنید 
-
-
-#### تنها مبدل توابع مهم 
-+ mysql2date
-+ get_the_modified_date
-+ get_post_modified_time
-
-
-#### ویژگی‌های افزونه
-+ فارسی ساز وردپرس 
-+ مبدل توابع مهم تاریخ
-+ تبدیل توابع اعداد به فارسی
-+ رفع مشکل هاست با زبان فارسی
-+ رفع خطای تاریخ سرور
-+ بسیار سبک و پایدار
+#### مبدل تاریخ میلادی وردپرس به خورشیدی
++ نصب زبان فارسی در صورت نصب نبودن
++ تبدیل اعداد انگلیسی به فارسی
++ رفع مشکل هاست با زبان و تاریخ
 + مکمل و سازگار با افزونه‌های مشابه
++ بسیار سبک، سریع، پایدار و ایمن
 
+هم در کنار این پلاگین استفاده کنید
+wp-jalali, wp-parsidate 
+**توجه:**
+میتوانید از امکانات پلاگین‌های
 
 == Installation ==
 
 آموزش نصب افزونه
 
-1.  Extract wp-farsi.zip to wp-content/plugins
-2.  Go to wp-admin/plugins.php then active WP-Farsi
+1.  Extract wp-farsi.zip to "wp-content/plugins" directory
+2.  Go to "wp-admin/plugins.php" url then activate WP-Farsi
 
 == Frequently Asked Questions ==
 
 = دلیل ساخت و کاربرد این پلاگین؟ =
 رفع نواقصی تبدیل تاریخ
 
-= تنظیمات پلاگین کجاست؟ =
-این افزونه تنظیمات خاصی ندارد و فقط یک گزینه جهت تنظیم ساختار اعداد دارد، در قسمت: مدیریت>تنظیمات>همگانی>ساختار اعداد
-
-
-= من نیاز به ویجت‌های تقویم+بایگانی و پیوندیکتا خورشیدی دارم؟ =
-میتوانید از امکانات پلاگین‌های مشابه هم در کنار این پلاگین استفاده کنید مانند wp-jalali و wp-parsidate
-
-
 == Screenshots ==
 
-1.  تنظیمات نمایش اعداد فارسی
 `/assets/screenshot-1.png`
+1.  تنظیم نمایش اعداد فارسی
 
 == Changelog ==
+
+= 2.0 =
+* Support WP 3.0.1 - 4.2
+
+= 1.8.2 =
+* Fixed a bug
+
+= 1.8.1 =
+* Fixed a bug
+
+= 1.8 =
+* Minor fix
+
+= 1.7 =
+* Small improvements
+
+= 1.6 =
+* Fixed admin date editor
+
+= 1.5 =
+* Improved Pdate lib
+* Better conversion
+* Fixed some bugs
+
+= 1.2 =
+* Small improvements
 
 = 1.1 =
 * Small improvements
@@ -70,4 +79,3 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
 = 1.0 =
 نیازی به آپگرید ندارد
-
--- wp-farsi.php
+++ wp-farsi.php
@@ -1,104 +1,106 @@
-<?php 
+<?php
 /**
-	Plugin Name: WP-Farsi
-	Plugin URI: http://wordpress.org/extend/plugins/wp-farsi
-	Description: افزونه مبدل تاریخ میلادی به شمسی، مکمل و سازگار با افزونه‌های مشابه. 
-	Author: Ali.Dbg
-	Author URI: https://github.com/alidbg/wp-farsi
-	Version: 1.1
-	License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
-*/ 
+Plugin Name: WP-Farsi
+Plugin URI: http://wordpress.org/extend/plugins/wp-farsi
+Description: افزونه مبدل تاریخ میلادی به شمسی، مکمل و سازگار با افزونه‌های مشابه.
+Author: Ali.Dbg
+Author URI: https://github.com/alidbg/wp-farsi
+Version: 2.0
+License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+*/
 
-defined('ABSPATH')||die;
+defined('ABSPATH') || exit;
+define('WPFA_NUMS', get_option('wpfa_nums'));
 
-if(extension_loaded('mbstring')){
+if (extension_loaded('mbstring')) {
     mb_internal_encoding('UTF-8');
     mb_language('neutral');
     mb_http_output('UTF-8');
-}elseif(function_exists('ini_set')){
-    ini_set('default_charset','UTF-8');
-    ini_set('date.timezone',  'UTC');
+} elseif (function_exists('ini_set')) {
+    ini_set('default_charset', 'UTF-8');
+    ini_set('date.timezone', 'UTC');
 }
 
-require_once plugin_dir_path( __FILE__ ) . 'pdate.php';
+require_once plugin_dir_path(__FILE__) . 'pdate.php';
 
-function num2fa($str,$cur=false){
- 	return str_replace(preg_split("//u","0123456789/,",12,1),preg_split("//u","۰۱۲۳۴۵۶۷۸۹٫".($cur?'٬':'،'),12,1),$str);
+function numbers_fa($str = '') {
+    return str_replace(range(0, 9), preg_split("//u", "۰۱۲۳۴۵۶۷۸۹", 10, 1), $str);
 }
 
-function download_faIR(){
-	$l = ABSPATH.'wp-admin/includes/translation-install.php';
-	if(file_exists($l)){require_once($l);wp_download_language_pack('fa_IR');}
-}
-
-function wpfa_activate(){
-    download_faIR();
-   	update_option('WPLANG', 'fa_IR');
+function wpfa_activate() {
+    update_option('WPLANG', 'fa_IR');
     update_option('start_of_week', '6');
     update_option('timezone_string', 'Asia/Tehran');
+    if(WPFA_NUMS === false) add_option('wpfa_nums', 'on');
+    $l = ABSPATH . 'wp-admin/includes/translation-install.php';
+    if (file_exists($l)) {require_once($l);wp_download_language_pack('fa_IR');}
 }
 
-function wpfa_deactivate(){
-	patch_func(false);
-	delete_option("wpfa_nums");
-}
-
-function post2pdate(){
+function wpfa_init() {
+    global $wp_locale;
+    $wp_locale->number_format['thousands_sep'] = ",";
+    $wp_locale->number_format['decimal_point'] = ".";
     if (isset($_POST['aa'], $_POST['mm'], $_POST['jj']))
         list($_POST['aa'], $_POST['mm'], $_POST['jj']) = jalali2gregorian(zeroise(intval($_POST['aa']), 4), zeroise(intval($_POST['mm']), 2), zeroise(intval($_POST['jj']), 2));
 }
 
-function patch_func($patch){
-	$source  = ABSPATH . 'wp-includes/functions.php';
-	$replace = "else\n\t\treturn date_i18n( " . '$format, $i' . " );";
-	$pattern = "else\n\t\treturn date( " . '$format, $i' . " );";
-	if(!$patch) list($replace, $pattern) = array($pattern, $replace);
-    if(is_readable($source) && is_writable($source)) file_put_contents($source, str_replace($pattern, $replace, file_get_contents($source)));
+function wpfa_patch_func($patch = false) {
+    $f = ABSPATH . 'wp-includes/functions.php';
+    if (!is_writable($f)) return;
+    $re = "/(else\\s+return\\s+(date.*[(]))/";
+    $src = file_get_contents($f);
+    if (preg_match($re, $src, $match) === 1){
+      if ($match[2] === "date(" && $patch)
+         file_put_contents($f, str_replace($match[1], "else\n\t\treturn date_i18n(", $src));
+      elseif ($match[2] === "date_i18n(" && !$patch)
+         file_put_contents($f, str_replace($match[1], "else\n\t\treturn date(", $src));
+    }
 }
 
-function timestampdiv(){?>
-<script>jQuery(document).ready(function(){jQuery("#timestampdiv").html(function(a,b){
-var c=("۰,۱,۲,۳,۴,۵,۶,۷,۸,۹,01-Jan,02-Feb,03-Mar,04-Apr,05-May,06-Jun,07-Jul,08-Aug,09-Sep,10-Oct,11-Nov,12-Dec").split(","),
-d=("0,1,2,3,4,5,6,7,8,9,01-فرو,02-ارد,03-خرد,04-تیر,05-مرد,06-شهر,07-مهر,08-آبا,09-آذر,10-دی,11-بهم,12-اسف").split(",");
-jQuery.each(c,function(a,c){b=b.replace(new RegExp(c,"gi"),d[a])});return b});
-jQuery("#mm option[value='"+jQuery('#hidden_mm').val()+"']").attr("selected","selected")})</script>
-<?php
+function timestampdiv() {?>
+<script type='text/javascript'>
+var c = ("۰,۱,۲,۳,۴,۵,۶,۷,۸,۹,Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec").split(",");
+var d = ("0,1,2,3,4,5,6,7,8,9,فرو,ارد,خرد,تیر,مرد,شهر,مهر,آبا,آذر,دی,بهم,اسف").split(",");
+jQuery(document).ready(function(){
+    jQuery("#timestampdiv,.timestamp-wrap,.inline-edit-date,.jj,.mm,.aa,.hh,.mn,.ss").html(function(a,b){
+    jQuery.each(c,function(a,c){b=b.replace(new RegExp(c,'g'),d[a])});return b});
+    jQuery("#mm option[value='"+jQuery('#hidden_mm').val()+"']").attr("selected","selected")
+});
+</script>
+<?php 
 }
 
 function dreg_jsfa() {
-	wp_dequeue_script('ztjalali_reg_admin_js');
-	wp_dequeue_script('ztjalali_reg_date_js');
-	wp_dequeue_script('wpp_admin');
+    wp_deregister_script('ztjalali_reg_admin_js');
+    wp_deregister_script('ztjalali_reg_date_js');
+    wp_deregister_script('wpp_admin');
 }
 
-function wpfa_load(){
-	if (num2fa(mysql2date("Y m", "2014 12", true)) != num2fa(mysql2date("Y m", "2014 12", false))) patch_func(true);
-
-	$remove_filters = array(
-		'date_i18n','get_post_time','get_comment_date','get_comment_time','get_the_date','the_date','get_the_time','the_time',
-		'get_the_modified_date','the_modified_date','get_the_modified_time','the_modified_time','get_post_modified_time','number_format_i18n'
-	); 
-	for ($i = 0; $i < sizeof($remove_filters); $i++) remove_all_filters($remove_filters[$i]);
-
-	add_filter('date_i18n', create_function('$g,$f,$t','return wpfa_pdate($f,intval($t));'), 10, 3);
-
-	if(get_option('wpfa_nums') === false) add_option('wpfa_nums', 'on');
-	if(get_option('wpfa_nums') == "on"){
-		add_filter('date_i18n', 'num2fa');
-		add_filter('number_format_i18n', create_function('$s','return num2fa($s,true);'));
-	}
+function wpfa_date_i18n($g = '', $f = '', $t = '') {
+    $d = wpfa_date($f,intval($t));
+    return WPFA_NUMS === "on" ? numbers_fa($d) : $d;
 }
 
-function wpfa_nums(){
+function wpfa_load() {
+    foreach (array(
+        'date_i18n', 'get_post_time', 'get_comment_date', 'get_comment_time', 'get_the_date', 'the_date', 'get_the_time', 'the_time',
+        'get_the_modified_date', 'the_modified_date', 'get_the_modified_time', 'the_modified_time', 'get_post_modified_time', 'number_format_i18n'
+    ) as $i) remove_all_filters($i);
+    if (mysql2date("Y m", "2014 12", true) !== mysql2date("Y m", "2014 12", false)) wpfa_patch_func(true);
+    if (WPFA_NUMS === "on") add_filter('number_format_i18n', 'numbers_fa');
+    add_filter('date_i18n', 'wpfa_date_i18n', 10, 3);
+}
+
+function wpfa_nums() {
     register_setting('general', 'wpfa_nums', 'esc_attr');
-    add_settings_field('wpfa_nums', '<label for="wpfa_nums">ساختار اعداد</label>', create_function('',
-    	'$val = get_option(\'wpfa_nums\');$checked = $val == "on" ? \' checked="checked"\' : "";
-		echo \'<label><input type="checkbox" id="wpfa_nums" name="wpfa_nums"\'.$checked.\' /> <span>فارسی ۰۱۲۳۴۵۶۷۸۹</span></label>\';'), 'general');
+    add_settings_field('wpfa_nums', '<label for="wpfa_nums">ساختار اعداد</label>', create_function('', '
+        echo \'<label><input type="checkbox" name="wpfa_nums" ' . (WPFA_NUMS === "on" ? "checked" : "") . '/> 
+        <span>فارسی ۰۱۲۳۴۵۶۷۸۹</span></label>\';'), 'general');
 }
 
-register_activation_hook(__FILE__ , 'wpfa_activate');
-register_deactivation_hook( __FILE__ , 'wpfa_deactivate');
-add_action('init', 'post2pdate');
+register_activation_hook(__FILE__, 'wpfa_activate');
+register_deactivation_hook(__FILE__, 'wpfa_patch_func');
+add_action('init', 'wpfa_init');
 add_action('admin_init', 'wpfa_nums');
 add_action('admin_footer', 'timestampdiv');
 add_action('wp_print_scripts', 'dreg_jsfa', 98);
